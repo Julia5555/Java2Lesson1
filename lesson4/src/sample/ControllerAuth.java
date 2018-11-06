43ac2400d7889963059bff66a9cd904ab09c3445
Julia Gladkova <gladkova.uv@gmail.com>
2018-11-06 17:43:49 +0500

    the chat is added

---
+++ lesson4/src/sample/ControllerAuth.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerAuth {
+
+    @FXML
+    TextField textAuth;
+
+
+    public void sendNick() throws IOException {
+        String text = textAuth.getText();
+        if(text == null || text.length() == 0)
+            return;
+
+        Stage stage = (Stage) textAuth.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("../sample/sample.fxml"));
+        Parent root = loader.load();
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setTitle(text + " chat");
+        Controller controller = loader.<Controller>getController();
+        controller.setUserNickName(text);
+    }
+
+
+}
