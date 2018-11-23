eff64eb37c3a424ce8ca496f7ef3aeef663822e6
Julia Gladkova <gladkova.uv@gmail.com>
2018-11-23 22:10:04 +0500

    unsubmited of time

---
+++ lesson8/src/client/Controller.java
@@ -0,0 +1,135 @@
+package client;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class Controller {
+
+    @FXML
+    TextField msgField;
+    @FXML
+    TextArea chatArea;
+    @FXML
+    HBox bottomPanel;
+    @FXML
+    HBox upperPanel;
+    @FXML
+    TextField loginField;
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    ListView<String> clientList;
+
+    private boolean isAuthorized;
+
+    Socket socket;
+    DataOutputStream out;
+    DataInputStream in;
+
+    final String IP_ADRESS = "localhost";
+    final int PORT = 8183;
+
+    public void setAuthorized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+        if (!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+            clientList.setVisible(false);
+            clientList.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+            clientList.setVisible(true);
+            clientList.setManaged(true);
+        }
+    }
+
+    void connect() {
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            setAuthorized(false);
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/authok")) {
+                            setAuthorized(true);
+                            break;
+                        } else {
+                            chatArea.appendText(str + "\n");
+                        }
+                    }
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/serverclosed")) break;
+                            if (str.startsWith("/clientlist")) {
+                                String[] tokens = str.split(" ");
+                                Platform.runLater(() -> {
+                                    clientList.getItems().clear();
+                                    for (int i = 1; i < tokens.length; i++) {
+                                        clientList.getItems().add(tokens[i]);
+                                    }
+                                });
+                            }
+                        }
+
+                        chatArea.appendText(str + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(msgField.getText());
+            msgField.clear();
+            msgField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tryToAuth() {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ lesson8/src/server/ClientHandler.java
@@ -0,0 +1,133 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler {
+
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private Server server;
+    private String nick;
+    private ArrayList<String> blackList;
+    private boolean isAuth;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public boolean isAuth() {
+        return isAuth;
+    }
+
+    public void setAuth(boolean auth) {
+        isAuth = auth;
+    }
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.blackList = new ArrayList<>();
+            this.socket = socket;
+            this.server = server;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.isAuth = false;
+
+            new Thread(() -> {
+                try {
+                    //цикл авторизации
+                    while (true) {
+                        new Thread(() ->{
+                            try {
+                                Thread.sleep(12000);
+                                if(!this.isAuth) {
+                                    System.out.println("unsubscribe client");
+                                    server.unsubscribe(this);
+                                }
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }).start();
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth")) {
+                            String[] tokens = str.split(" ");
+                            String newNick = AuthService.getNickname(tokens[1], tokens[2]);
+
+                            if (newNick != null) {
+                                if (!server.isNickBusy(newNick)) {
+                                    sendMsg("/authok");
+                                    nick = newNick;
+                                    server.subscribe(this);
+                                    break;
+                                } else sendMsg("Учетная запись уже используется!");
+                            }
+                        } else sendMsg("Неверный логин/пароль");
+                    }
+                    //рабочий цикл
+                    while (true) {
+                        String str = in.readUTF();
+                        //служебные сообщения
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                out.writeUTF("/serverclosed");
+                                break;
+                            }
+                            if (str.startsWith("/w ")) {
+                                String[] tokens = str.split(" ", 3);
+                                server.sendPersonalMsg(this, tokens[1], tokens[2]);
+                            }
+                            if (str.startsWith("/blacklist ")) {
+                                String[] tokens = str.split(" ");
+                                blackList.add(tokens[1]);
+                                sendMsg("Вы добавили пользователя с ником " + tokens[1] + " в черный список!");
+                            }
+                        } else {
+                            server.broadcastMsg(this, nick + " " + str);
+                        }
+                        System.out.println("Client " + str);
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkBlackList(String nick) {
+        return blackList.contains(nick);
+    }
+}
